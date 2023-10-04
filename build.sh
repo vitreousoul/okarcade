6ec09e3171f39f3c1adcb587b4da3a009b7b5aa7
Ryan Merritt <merrittaudio@gmail.com>
2023-10-04 14:58:01 -0500

    just get something running for now

---
+++ build.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env sh
+
+# GRAPHICS_FRAMEWORKS="-framework CoreVideo -framework IOKit -framework Cocoa -framework GLUT -framework OpenGL"
+# GRAPHICS_LIB="clibs/libraylib.a"
+
+DEBUG=1
+SETTINGS="-std=c99 -Wall -Wextra -Wstrict-prototypes -Wold-style-definition -Wmissing-prototypes -Wmissing-declarations -Wno-comment"
+SOURCE_FILE="./src/main.c"
+
+if [ ! -d "dist" ]; then
+    mkdir dist
+fi
+
+if [ $DEBUG -eq 0 ]; then
+    echo "Optimized build";
+    TARGET="-O2 -o dist/main.exe"
+elif [ $DEBUG -eq 1 ]; then
+    echo "Debug build";
+    TARGET="-g3 -O0 -o dist/main.out"
+fi
+
+if [ ! -d "dist" ]; then
+  mkdir dist
+fi
+
+gcc $FRAMEWORKS $RAY_LIB $SETTINGS $SOURCE_FILE $TARGET
---
+++ src/main.c
@@ -0,0 +1,189 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#define internal static
+#define global_variable static
+
+typedef uint8_t u8;
+typedef uint32_t u32;
+
+typedef uint32_t b32;
+
+typedef int32_t s32;
+
+typedef size_t size;
+
+typedef s32 bar;
+
+#define EXPANSION_BUFFER_SIZE 1 << 12
+#define EXPANSION_MAX_DEPTH 6
+#define RULE_SIZE_MAX 16
+
+typedef enum
+{
+    symbol_Undefined,
+    symbol_Root,
+    symbol_A,
+    symbol_B,
+    symbol_Count,
+} symbol;
+
+typedef struct
+{
+    symbol Symbol;
+    s32 Index;
+} expansion_item;
+
+typedef struct
+{
+    s32 Index;
+    expansion_item Items[EXPANSION_MAX_DEPTH];
+} expansion;
+
+internal expansion_item CreateExpansionItem(symbol Symbol, s32 Index)
+{
+    expansion_item Item;
+
+    Item.Symbol = Symbol;
+    Item.Index = Index;
+
+    return Item;
+}
+
+internal void PushExpansionItem(expansion *Expansion, expansion_item Item)
+{
+    if (Expansion->Index >= 0 && Expansion->Index < EXPANSION_MAX_DEPTH)
+    {
+        Expansion->Items[Expansion->Index] = Item;
+        Expansion->Index += 1;
+    }
+}
+
+internal expansion_item PopExpansionItem(expansion *Expansion)
+{
+    if (Expansion->Index < 0)
+    {
+        return CreateExpansionItem(symbol_Undefined, -1);
+    }
+    else
+    {
+        expansion_item Item;
+        Item.Symbol = Expansion->Items[Expansion->Index].Symbol;
+        Item.Index = Expansion->Items[Expansion->Index].Index;
+        Expansion->Index -= 1;
+
+        return Item;
+    }
+}
+
+internal expansion_item PeekExpansionItem(expansion *Expansion)
+{
+    s32 PeekIndex = Expansion->Index - 1;
+
+    if (PeekIndex < 0 || PeekIndex >= EXPANSION_MAX_DEPTH)
+    {
+        return CreateExpansionItem(symbol_Undefined, -1);
+    }
+    else
+    {
+        expansion_item Item;
+        Item.Symbol = Expansion->Items[PeekIndex].Symbol;
+        Item.Index = Expansion->Items[PeekIndex].Index;
+
+        return Item;
+    }
+}
+
+internal symbol GetSymbolFromRules(symbol Rules[symbol_Count][RULE_SIZE_MAX], expansion_item Item)
+{
+    b32 RuleIndexInBounds = Item.Index >= 0 && Item.Index < RULE_SIZE_MAX;
+
+    if (RuleIndexInBounds)
+    {
+        return Rules[Item.Symbol][Item.Index];
+    }
+    else
+    {
+        return symbol_Undefined;
+    }
+}
+
+internal void PrintLSystem(symbol Rules[symbol_Count][RULE_SIZE_MAX], s32 Depth)
+{
+    expansion Expansion = {0};
+
+    {
+        expansion_item RootItem = CreateExpansionItem(symbol_Root, 0);
+        PushExpansionItem(&Expansion, RootItem);
+    }
+
+    s32 FOO = 0;
+
+    while (Expansion.Index > 0)
+    {
+        if (FOO++ > 100) {printf("max iter count in PrintLSystem\n"); break;}
+        expansion_item CurrentItem = PeekExpansionItem(&Expansion);
+        b32 RuleIndexInBounds = CurrentItem.Index >= 0 && CurrentItem.Index < RULE_SIZE_MAX;
+
+        if (!RuleIndexInBounds)
+        {
+            printf("Rule index out of bounds\n");
+            break;
+        }
+
+        if (CurrentItem.Symbol == symbol_Undefined)
+        {
+            PopExpansionItem(&Expansion);
+        }
+        else if (Expansion.Index <= Depth)
+        {
+            symbol ChildSymbol = Rules[CurrentItem.Symbol][CurrentItem.Index];
+            expansion_item ChildItem = CreateExpansionItem(ChildSymbol, 0);
+
+            Expansion.Items[Expansion.Index].Index += 1;
+            PushExpansionItem(&Expansion, ChildItem);
+        }
+        else
+        {
+            /* output symbols */
+            symbol OutputSymbol = GetSymbolFromRules(Rules, CurrentItem);
+
+            while (OutputSymbol != symbol_Undefined)
+            {
+                switch (OutputSymbol)
+                {
+                case symbol_A: {printf("a");} break;
+                case symbol_B: {printf("b");} break;
+                case symbol_Undefined: {printf("_");} break;
+                default: break;
+                }
+
+                CurrentItem.Index += 1;
+                OutputSymbol = GetSymbolFromRules(Rules, CurrentItem);
+            }
+
+            PopExpansionItem(&Expansion);
+        }
+    }
+
+    printf("\n");
+}
+
+int main(void)
+{
+    int Result = 0;
+
+    symbol A = symbol_A;
+    symbol B = symbol_B;
+    symbol End = symbol_Undefined;
+
+    symbol Rules[symbol_Count][RULE_SIZE_MAX] = {
+        [symbol_Root] = {A,B,A,B,End},
+        [symbol_A] = {A,B,A,End},
+        [symbol_B] = {B,B,End},
+    };
+
+    PrintLSystem(Rules, 1);
+
+    return Result;
+}
