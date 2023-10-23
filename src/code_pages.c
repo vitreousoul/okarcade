0e1bbeb566c1ccc3f7b34220925d0c5e1a282cbe
Ryan Merritt <merrittaudio@gmail.com>
2023-10-23 13:17:00 -0500

    sort file list

--- src/code_pages.c
+++ src/code_pages.c
@@ -1,13 +1,100 @@
 void GenerateCodePages(void);
 
+internal s32 CompareString(u8 *StringA, u8 *StringB)
+{
+    s32 Result = 0;
+    s32 I = 0;
+
+    for (;;)
+    {
+        if (StringA[I] < StringB[I])
+        {
+            Result = -1;
+            break;
+        }
+        else if (StringB[I] < StringA[I])
+        {
+            Result = 1;
+            break;
+        }
+
+        if (!(StringA[I] && StringB[I]))
+        {
+            break;
+        }
+
+        I += 1;
+    }
+
+    return Result;
+}
+
+internal file_list *SortFileList(file_list *Files)
+{
+    file_list *UnsortedFiles = Files->Next;
+    file_list *SortedFiles = Files;
+    SortedFiles->Next = 0;
+
+    while (UnsortedFiles)
+    {
+        file_list *CurrentSortedFile = SortedFiles;
+        file_list *PreviousSortedFile = 0;
+
+        while (CurrentSortedFile)
+        {
+            s32 CompareResult = CompareString(UnsortedFiles->Name, CurrentSortedFile->Name);
+
+            if (CompareResult < 0)
+            {
+                if (!PreviousSortedFile)
+                {
+                    /* current sorted file is head of list */
+                    file_list *NextUnsortedFile = UnsortedFiles->Next;
+                    UnsortedFiles->Next = CurrentSortedFile;
+                    SortedFiles = UnsortedFiles;
+                    UnsortedFiles = NextUnsortedFile;
+                }
+                else
+                {
+                    file_list *NextUnsortedFile = UnsortedFiles->Next;
+                    PreviousSortedFile->Next = UnsortedFiles;
+                    UnsortedFiles->Next = CurrentSortedFile;
+                    UnsortedFiles = NextUnsortedFile;
+                }
+
+                break;
+            }
+            else if (!CurrentSortedFile->Next)
+            {
+                /* unsorted file is greater than all sorted files */
+                file_list *NextUnsortedFile = UnsortedFiles->Next;
+                CurrentSortedFile->Next = UnsortedFiles;
+                UnsortedFiles->Next = 0;
+                UnsortedFiles = NextUnsortedFile;
+                break;
+            }
+            else
+            {
+                PreviousSortedFile = CurrentSortedFile;
+                CurrentSortedFile = CurrentSortedFile->Next;
+            }
+        }
+
+
+    }
+
+    return SortedFiles;
+}
+
 void GenerateCodePages(void)
 {
     linear_allocator FileAllocator = WalkDirectory((u8 *)"../src");
     file_list *FileList = (file_list *)FileAllocator.Data;
+    file_list *SortedFileList = SortFileList(FileList);
 
-    for (; FileList; FileList = FileList->Next)
+    for (; SortedFileList; SortedFileList = SortedFileList->Next)
     {
-        printf("%s\n", FileList->Name);
+        printf("%s\n", SortedFileList->Name);
     }
 
     FreeLinearAllocator(FileAllocator);
